d91c90e8809e2000bb138068d0af968a5e5d3bab
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-11-17 05:23:24 +0900

    選んだ音源を再生、ざっくりしきい値を撮り続けることに成功

---
+++ public/js/hueph_volume.js
@@ -0,0 +1,97 @@
+/* functions of WebAudioAPI */
+init = function() {
+  var dfd = $.Deferred();
+  try {
+    var AudioContext = new window.AudioContext() || new window.webkitAudioContext();
+    dfd.resolve(AudioContext);
+    return dfd.promise();
+  } catch(e) {
+    dfd.reject(e);
+    return dfd.promise();
+  }
+}
+
+loadSound = function(context, url) {
+  return new Promise(function(resolve, reject) {
+  var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.send();
+    request.onload = function() {
+      context.decodeAudioData(request.response, function(buffer) {
+        resolve(buffer);
+      });
+    }
+  });
+}
+
+makeSource = function(context, buffer, analyser) {
+  source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.connect(analyser);
+  return source;
+}
+
+playSound = function(source) {
+  source.start(0);
+}
+
+stopSound = function(source) {
+  source.stop();
+}
+
+/* for Hue */
+ip   = null;
+user = "newdeveloper";
+/* for WebAudioAPI */
+context  = null;
+source   = null;
+buffer   = null;
+url      = null;
+analyser = null;
+getFreq  = null;
+mode     = 0;
+
+getFreq = function() {
+  data = new Uint8Array(256);
+  console.log('hello');
+  if(mode===1) analyser.getByteFrequencyData(data);
+  var sum = 0;
+  for(var i=0;i<256;i++) {
+    sum += data[i];
+  }
+  console.log(sum/256);
+}
+
+window.onload = function() {
+  init()
+    .then(function(result) {
+      context = result;
+      analyser = context.createAnalyser();
+      analyser.fftsize = 1024;
+      analyser.smoothingTimeContant = 0.9;
+    })
+    .fail(function(error) {
+      console.log(error);
+    });
+}
+
+$(function() {
+  $('.go').click(function() {
+    var music = $("select[name='music']").val();
+    var url = location.origin + '/music/' + music;
+    loadSound(context, url)
+      .then(function(buf) {
+        buffer = buf;
+        source = makeSource(context, buffer, analyser);
+        playSound(source);
+        mode = 1;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
+});
+
+setInterval(getFreq, 100);
